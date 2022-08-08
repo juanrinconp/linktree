089f64d12ffaef14be0a9ea1bbf46698e5be19d5
Juan Esteban Rincón Poveda <66634832+juanrinconp@users.noreply.github.com>
2022-08-08 12:13:56 -0500

    Add files via upload

---
+++ assets/config.js
@@ -0,0 +1,32 @@
+let config = {
+  "Title": "Juan Esteban Rincón Poveda",
+  "Name": "Juan Esteban Rincón Poveda",
+  "Description": "Links",
+  "links": [
+    {
+      "Title": "Website",
+      "URL": "https://juanrinconp.github.io/website/",
+      "icon_classes": "fas fa-link",
+    },
+    {
+      "Title": "Photos",
+      "URL": "https://juanrinconp.github.io/album/",
+      "icon_classes": "fab fa-leanpub"
+    },
+    {
+      "Title": "LinkedIn",
+      "URL": "https://www.linkedin.com/in/harindulk/",
+      "icon_classes": "fab fa-linkedin"
+    },
+    {
+      "Title": "Youtube",
+      "URL": "https://www.youtube.com/channel/UCwWW8vfwtnyr3xeUFMR4Gjg",
+      "icon_classes": "fab fa-youtube"
+    },
+    {
+      "Title": "Github",
+      "URL": "https://www.linkedin.com/in/juanrinconp/",
+      "icon_classes": "fab fa-github",
+    },
+  ]
+}
